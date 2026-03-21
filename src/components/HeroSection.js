cbf7bb8f8e9bd90c057fc7f1f4b0c95451132722
augustine-ugwu <augustinehills@yahoo.com>
2026-03-21 14:11:33 +0100

    Fixed HeroSection for other pages

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const HeroSection = ({ title, backgroundImage }) => {
   return (
-    <div
-      className="relative h-[250px] bg-cover bg-center"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+    <div className="relative h-[250px] overflow-hidden">
+      {/* Background Layer */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      />
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/50" />
+
+      {/* Content */}
       <div className="relative z-10 flex items-center justify-center h-full">
         <h1 className="font-heading text-white text-4xl lg:text-5xl text-center">
           {title}
